a2254936be5a14d8a6ca182f699f71e754f7c7b6
Pedro Lira <pedrohml@gmail.com>
2015-12-04 18:16:31 -0200

    Add usage info to env script

--- env.sh
+++ env.sh
@@ -3,3 +3,11 @@
 if [[ ! -d ENV ]]; then
 	virtualenv ENV
 fi
+
+. ENV/bin/activate
+pip install -r requirements.txt
+
+echo
+echo Now you can type:
+printf "\e[0;32msource ENV/bin/activate\n"
+echo
