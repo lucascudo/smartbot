084fab43a493c0aa607dee688227e58921003674
Pedro Lira <pedrohml@gmail.com>
2015-12-11 21:58:07 +0000

    Fix encode for external api queries

--- smartbot/joke_behaviour.py
+++ smartbot/joke_behaviour.py
@@ -25,7 +25,7 @@ class JokeBehaviour(Behaviour):
         p = re.compile('([^ ]*) (.*)')
         query = (p.match(update.message.text).groups()[1] or '').strip()
         self.logDebug(u'Joke search (chat_id: %s, query: %s)' % (update.message.chat_id, query or 'None'))
-        jokes = ExternalAPI.searchJoke(query)
+        jokes = ExternalAPI.searchJoke(query.encode('utf-8'))
         if jokes:
             telegramBot.sendMessage(chat_id=update.message.chat_id, text=random.choice(jokes))
 
@@ -33,13 +33,13 @@ class JokeBehaviour(Behaviour):
         p = re.compile('([^ ]*) (.*)')
         query = (p.match(update.message.text).groups()[1] or '').strip()
         self.logDebug(u'Jalk search (chat_id: %s, query: %s)' % (update.message.chat_id, query or 'None'))
-        jokes = ExternalAPI.searchJoke(query)
+        jokes = ExternalAPI.searchJoke(query.encode('utf-8'))
         if jokes:
             jokes = filter(lambda c: len(re.split('\W+', c, re.MULTILINE)) < 200, jokes)
             jokes = sorted(jokes, lambda x, y: len(x) - len(y))
             if jokes:
                 joke = jokes[0]
-                audioFile = ExternalAPI.textToSpeech(joke, language='pt', encode='mp3')
+                audioFile = ExternalAPI.textToSpeech(joke.encode('utf-8'), language='pt', encode='mp3')
                 if os.path.exists(audioFile) and os.path.getsize(audioFile) > 0:
                     self.bot.sendAudio(chat_id=update.message.chat_id, audio=audioFile, performer=self.bot.getInfo().username)
                 else:
