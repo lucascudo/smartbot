b00eaf76b868b44d3cee969f0c7f6e3cc5e6acf4
Pedro Lira <pedrohml@gmail.com>
2015-12-11 02:43:57 -0200

    Add bing image search

--- smartbot/bing_image_behaviour.py
+++ smartbot/bing_image_behaviour.py
@@ -3,22 +3,22 @@
 from smartbot import Behaviour
 from smartbot import ExternalAPI
 
-class GoogleImageBehaviour(Behaviour):
+class BingImageBehaviour(Behaviour):
     def __init__(self, bot):
-        super(GoogleImageBehaviour, self).__init__(bot)
+        super(BingImageBehaviour, self).__init__(bot)
         self.dispatcher = bot.dispatcher;
 
     def addHandlers(self):
-        self.dispatcher.addTelegramCommandHandler('gimage', self.imageSearch)
+        self.dispatcher.addTelegramCommandHandler('bimage', self.imageSearch)
 
     def removeHandlers(self):
-        self.dispatcher.removeTelegramCommandHandler('gimage', self.imageSearch)
+        self.dispatcher.removeTelegramCommandHandler('bimage', self.imageSearch)
 
     def imageSearch(self, telegramBot, update):
         p = re.compile('([^ ]*) (.*)')
         query = (p.match(update.message.text).groups()[1] or '').strip()
-        self.logDebug(u'Google image search (chat_id: %s, query: %s)' % (update.message.chat_id, query or 'None'))
-        imageSources = ExternalAPI.searchGoogleImage(query)
+        self.logDebug(u'Bing image search (chat_id: %s, query: %s)' % (update.message.chat_id, query or 'None'))
+        imageSources = ExternalAPI.searchBingImage(query)
         if imageSources:
             telegramBot.sendMessage(chat_id=update.message.chat_id, text=imageSources[0])
         else:
