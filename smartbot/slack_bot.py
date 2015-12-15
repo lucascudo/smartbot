03968fb6ab81ee874159d381869877d0481bb8ba
Pedro Lira <pedrohml@gmail.com>
2015-12-15 02:39:53 -0200

    Improve slack bot response

--- smartbot/slack_bot.py
+++ smartbot/slack_bot.py
@@ -4,6 +4,7 @@ import io
 import re
 import sys
 import time
+import json
 import requests
 import slackclient
 from smartbot import Bot
@@ -13,9 +14,8 @@ class DynObject(object):
     pass
 
 class SlackBot(Bot):
-    def __init__(self, token, name='bot'):
+    def __init__(self, token):
         super(SlackBot, self).__init__(token)
-        self.name = name
         self.slackClient = slackclient.SlackClient(token)
         self._commandMatcher = re.compile('^_(\w+)( .*)?$')
         self._messageHandlers = []
@@ -24,9 +24,12 @@ class SlackBot(Bot):
         self.info = None
 
     def getInfo(self):
-        info = DynObject()
-        info.username = info.first_name = self.name
-        return info
+        if not self.info:
+            slackInfo = json.loads(self.slackClient.api_call('auth.test'))
+            self.info = DynObject()
+            self.info.id = slackInfo.get('user_id')
+            self.info.username = self.info.first_name = slackInfo.get('user')
+        return self.info
 
     def addMessageHandler(self, handler):
         self._messageHandlers.append((handler,))
@@ -50,7 +53,7 @@ class SlackBot(Bot):
         channel = kargs.get('chat_id')
         text = kargs.get('text') or ''
         Utils.logDebug(self, self.__class__.__name__, 'sendMessage %s' % kargs)
-        self.slackClient.api_call('chat.postMessage', channel=channel, text=text.encode('utf-8'))
+        self.slackClient.api_call('chat.postMessage', channel=channel, username=self.getInfo().username, icon_emoji=':robot_face:', text=text.encode('utf-8'))
 
     def sendVoice(self, **kargs):
         Utils.logDebug(self, self.__class__.__name__, 'sendVoice %s' % kargs)
@@ -59,35 +62,44 @@ class SlackBot(Bot):
         Utils.logDebug(self, self.__class__.__name__, 'sendAudio %s' % kargs)
 
     def dispatchMessage(self, update):
+        dispatched = False
         for messageHandler in self._messageHandlers:
             handler = messageHandler[0]
             handler(self, update)
+            dispatched = True
+        return dispatched
 
     def dispatchRegex(self, update):
+        dispatched = False
         for regexHandler in self._regexHandlers:
             regex = regexHandler[0]
             handler = regexHandler[1]
             if regex.match(update.message.text):
                 handler(self, update)
+                dispatched = True
+        return dispatched
 
     def dispatchCommand(self, update, command=None):
+        dispatched = False
         commandMatches = self._commandMatcher.match(update.message.text)
         if command or commandMatches:
             for commandHandler in self._commandHandlers:
-                command = command or commandHandler[0]
+                commandCurrent = commandHandler[0]
                 handler = commandHandler[1]
-                if command == commandMatches.groups()[0]:
+                if command == commandCurrent or commandCurrent == commandMatches.groups()[0]:
                     handler(self, update)
+                    dispatched = True
+        return dispatched
 
     def processUpdate(self, update):
         if update.type == 'message':
-            self.dispatchMessage(update)
-            self.dispatchRegex(update)
-            self.dispatchCommand(update)
+            if not self.dispatchCommand(update):
+                self.dispatchMessage(update)
+                self.dispatchRegex(update)
 
     def convertToUpdate(self, slackEvent):
         update = DynObject()
-        if slackEvent.get(u'type') == 'message':
+        if slackEvent.get(u'type') == 'message' and not slackEvent.get(u'subtype') == 'bot_message':
             update.type = 'message'
             update.message = DynObject()
             update.message.text = slackEvent.get(u'text') or ''
@@ -97,13 +109,12 @@ class SlackBot(Bot):
         return update
 
     def listen(self):
-        mentionMatcher = re.compile('.*(^|\W)(%s)($|\W).*' % self.name, re.IGNORECASE)
         try:
             if self.slackClient.rtm_connect():
                 while True:
                     events = self.slackClient.rtm_read()
                     for event in events:
                         self.processUpdate(self.convertToUpdate(event))
-                    time.sleep(1)
+                    time.sleep(0.5)
         except:
             Utils.logError(self, self.__class__.__name__, sys.exc_info())
